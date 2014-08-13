f97dc9f9e45ccd603bafd359c35e2f22c6f13ad1
HydraOrc <hydra0@bigmir.net>
2014-08-13 14:21:56 +0300

    yandex metrika out of core.js

--- public/js/user/core.js
+++ public/js/user/core.js
@@ -35,31 +35,6 @@ $(function() {
 	}
 });
 
-<!-- Yandex.Metrika counter -->
-(function (d, w, c) {
-    (w[c] = w[c] || []).push(function() {
-        try {
-            w.yaCounter25778882 = new Ya.Metrika({id:25778882,
-                    webvisor:true,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true});
-        } catch(e) { }
-    });
-
-    var n = d.getElementsByTagName("script")[0],
-        s = d.createElement("script"),
-        f = function () { n.parentNode.insertBefore(s, n); };
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-    if (w.opera == "[object Opera]") {
-        d.addEventListener("DOMContentLoaded", f, false);
-    } else { f(); }
-})(document, window, "yandex_metrika_callbacks");
-<!-- /Yandex.Metrika counter -->
-
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-53500731-1']);
 _gaq.push(['_setSiteSpeedSampleRate', 20]);
